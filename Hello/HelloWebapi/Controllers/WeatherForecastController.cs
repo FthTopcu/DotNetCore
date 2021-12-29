b9ae67e4430dc0f84430d1f6748613bf3907eb3f
Fatih Topcu <fthtpu@gmail.com>
2021-12-29 11:01:00 +0300

    weather

--- Hello/HelloWebapi/Controllers/WeatherForecastController.cs
+++ Hello/HelloWebapi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace HelloWebapi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]s")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -35,5 +35,38 @@ namespace HelloWebapi.Controllers
             })
             .ToArray();
         }
+
+
+         [HttpGet("{id}")]
+        //api/weatherForecasts/3
+        public ActionResult<WeatherForecast> GetById(string id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray()[0];
+        }
+    
+
+        //---------örnek
+        //[HttpGet]
+        //api/weatherForecasts?id=3
+        // public IEnumerable<WeatherForecast> GetById([FromQuery] string id)
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+        
+       
     }
 }
